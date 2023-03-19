0b3663983decebf4889be1df3c93668aa404d5de
DamTran <thdam3012@gmail.com>
2023-03-19 13:36:36 +0200

    second commit

--- scoreboard.py
+++ scoreboard.py
@@ -10,21 +10,29 @@ class ScoreBoard(Turtle):
         self.penup()
         self.color("white")
         self.goto(0, 270)
+        self.high_score = 0
         self.score = 0
         self.hideturtle()
         self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
 
-    def update_score(self):
-        self.clear()
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
 
 
 
