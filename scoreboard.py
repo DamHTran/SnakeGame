c66e347a02034f7d42a20a7e3fb58ec131662526
DamTran <thdam3012@gmail.com>
2023-03-19 14:49:06 +0200

    third commit

--- scoreboard.py
+++ scoreboard.py
@@ -10,7 +10,8 @@ class ScoreBoard(Turtle):
         self.penup()
         self.color("white")
         self.goto(0, 270)
-        self.high_score = 0
+        with open("data.txt") as data:
+            self. high_score = int(data.read())
         self.score = 0
         self.hideturtle()
         self.update_score()
@@ -30,8 +31,11 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
+
 
 
 
